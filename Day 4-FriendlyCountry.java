81e89eabe3752cfcd189bb758822e6b4c71c48ab
RishikaPotepalli <82092914+RishikaPotepalli@users.noreply.github.com>
2021-04-07 18:07:24 +0530

    Add files via upload

---
+++ Day 4-FriendlyCountry.java
@@ -0,0 +1,16 @@
+package k;
+
+
+/* No International Flyer Tax on these countries, Considering as Free Travel*/
+public class FriendlyCountry {
+
+	public static String countries[] =
+		{
+				"UAE",
+				"Nepal",
+				"Bhutan",
+				"Mauritius",
+				"Afganistan",
+				"Mangolia",
+		};
+}
